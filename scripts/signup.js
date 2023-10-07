d1978432d404350eab8e4c5e3c1a497af7d44fe4
Duong Nam Phong <namphong28042010@gmail.com>
2023-10-07 19:31:23 +0700

    Update

--- scripts/signup.js
+++ scripts/signup.js
@@ -1,3 +1,20 @@
+window.onload = function () {
+    var loader = document.querySelector('.loader');
+    setTimeout(function () {
+        loader.style.transition = "opacity 0.5s";
+        loader.style.opacity = "0";
+        setTimeout(function () {
+            loader.style.display = "none";
+        }, 500);
+    }, 2000);
+};
+// JavaScript để xử lý sự kiện cho nút menu di động
+const menuIcon = document.querySelector(".menu-icon");
+const navbarItems = document.querySelector(".navbar-items");
+
+menuIcon.addEventListener("click", () => {
+    navbarItems.classList.toggle("active");
+});
 document.getElementById('signupForm').addEventListener('submit', function (e) {
     e.preventDefault();
 
