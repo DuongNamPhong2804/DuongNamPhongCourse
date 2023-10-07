199492a124079a575fc6a072ff755efab1f29048
Duong Nam Phong <namphong28042010@gmail.com>
2023-10-07 20:44:59 +0700

    Update

--- scripts/course.js
+++ scripts/course.js
@@ -1,3 +1,13 @@
+window.onload = function () {
+    var loader = document.querySelector('.loader');
+    setTimeout(function () {
+        loader.style.transition = "opacity 0.5s";
+        loader.style.opacity = "0";
+        setTimeout(function () {
+            loader.style.display = "none";
+        }, 500);
+    }, 2000);
+};
 // JavaScript để xử lý sự kiện cho nút menu di động
 const menuIcon = document.querySelector(".menu-icon");
 const navbarItems = document.querySelector(".navbar-items");
